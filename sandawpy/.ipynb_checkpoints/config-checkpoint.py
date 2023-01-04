4be7f7b58a664765fd9ccee4374daaa5306c5377
JYangQi00 <jiq019@ucsd.edu>
2023-01-04 15:36:46 -0800

    Removed ipynb_checkpoints

--- sandawpy/.ipynb_checkpoints/config-checkpoint.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import numba
-import json
-import configparser
-
-def LoadConfig(config_file):
-    cfg = configparser.ConfigParser()
-    cfg.read(config_file)
-    return cfg
--- sandawpy/.ipynb_checkpoints/loader-checkpoint.py
+++
@@ -1,9 +0,0 @@
-from . import peaks
-
-class Loader():
-    def __init__(self, config_file):
-        self.peaks = peaks.Peaks(config_file)
-        self.datatype_dict = {'peaks' : self.peaks}
-        
-    def GetData(self, file, data_type, **kwargs):
-        return self.datatype_dict[data_type].Load(file, **kwargs)
